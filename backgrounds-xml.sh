05a2078653c90508a9a91a9c3517dbe46bc6877c
Ste74 <capitani74@gmail.com>
2017-10-25 22:01:13 +0200

    Renamed wallpaper and clean the script

--- backgrounds-xml.sh
+++ backgrounds-xml.sh
@@ -2,32 +2,8 @@
 
 #set variable
 
-set -x
-
 DE=gnome
-#DIRECTORY=$(pwd)
 ABSPATH=/usr/share/backgrounds/manjaro-$DE/
-
-clear
-echo "#################################"
-echo "This script makes all pictures in selected custom directory"
-echo "available as your system-wide $DE wallpapers." 
-echo "It reads/lists all pictures in selected directory and creates manjaro-$DE.xml file in /usr/share/gnome-background-properties/."
-echo "#################################"
-echo
-#if [ ! -d $DIRECTORY ]
-#then
-#    echo "Directory $DIRECTORY doesnot exists"
-#    echo "You have to start the Script again."	
-#    exit
-#fi
-
-#echo "You selected $DIRECTORY:"
-#ls $DIRECTORY > lspictures.txt
-#cat lspictures.txt
-#echo
-
-#check if mybackground.xml already exist
 
 if [ -e manjaro-$DE.xml ]; then
     rm manjaro-$DE.xml
@@ -56,12 +32,6 @@ done
 # creating the bottom of mybackgrounds.xml
 echo "</wallpapers>" >> manjaro-$DE.xml
 
-# Change <name>/path_to/picture</name> to <name>picture</name> and move mybackgrounds.xml  to $srcdir/share/gnome-background-properties/.
-# if you like to copy mybackgrounds.xml to $srcdir/mybackgrounds.xml as well
-#sed "s|${DIRECTORY}/|${ABSPATH}/|g" manjaro-$DE.xml > ${DIRECTORY}/manjaro-$DE.xml
-
-#clean all
-#rm lspictures.txt
 echo
 echo "#################################"
 echo "DONE xml file created"
